382ae6d02bcc298c69b8e2d606a2609029d1d40c
paul Haratyk <paulo@iPaul-VIII.local>
2018-12-01 01:51:10 +0100

    nik_ptn

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pharatyk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/29 16:17:19 by pharatyk          #+#    #+#             */
+/*   Updated: 2018/11/29 16:17:29 by pharatyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1000
+
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "libft.h"
+
+
+int		get_next_line(const int fd, char **line);
+
+#endif
